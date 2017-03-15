dbc82f15786da6cfe18536d83df7d1d95572fa5c
Yaroslav Yermilov <yermilov17@gmail.com>
2017-03-15 12:35:39 +0200

    #3. time limit functionality

--- src/main/groovy/com/github/vuzoll/tasks/service/JobsService.groovy
+++ src/main/groovy/com/github/vuzoll/tasks/service/JobsService.groovy
@@ -158,14 +158,20 @@ class JobsService {
 
                 job = jobRepository.findOne job.id
 
+                if (durableJob.finished) {
+                    job.lastMessage = 'finished successfully'
+                    log.info "${jobLogPrefix(job.id)} ${job.lastMessage}"
+                    break
+                }
+
                 if (job.status == JobStatus.STOPPING.toString()) {
                     job.lastMessage = 'stopped by client request'
                     log.info "${jobLogPrefix(job.id)} ${job.lastMessage}"
                     break
                 }
 
-                if (durableJob.finished) {
-                    job.lastMessage = 'finished successfully'
+                if (durableJob.timeLimit != null && System.currentTimeMillis() - job.startTimestamp > fromDurationString(durableJob.timeLimit)) {
+                    job.lastMessage = 'time limit is exceeded'
                     log.info "${jobLogPrefix(job.id)} ${job.lastMessage}"
                     break
                 }
